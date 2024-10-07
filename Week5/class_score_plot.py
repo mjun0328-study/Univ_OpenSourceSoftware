d363e3ba3e89a2bdc6fd77603c717dc76d8414b7
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-10-07 11:05:20 +0900

    Week5 Practice

--- Week5/class_score_plot.py
+++ Week5/class_score_plot.py
@@ -1,8 +1,9 @@
+import os
 import matplotlib.pyplot as plt
 
 def read_data(filename):
     data = []
-    with open(filename, 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as f:
         for line in f.readlines():
             if not line.startswith('#'): # If 'line' is not a header
                 data.append([int(word) for word in line.split(',')])
@@ -13,14 +14,29 @@ if __name__ == '__main__':
     class_kr = read_data('data/class_score_kr.csv')
     class_en = read_data('data/class_score_en.csv')
 
-    # TODO) Prepare midterm, final, and total scores
+    # Prepare midterm, final, and total scores
     midterm_kr, final_kr = zip(*class_kr)
     total_kr = [40/125*midterm + 60/100*final for (midterm, final) in class_kr]
-    midterm_en, final_en = [0, 0]
-    total_en = [0]
+    midterm_en, final_en = zip(*class_en)
+    total_en = [40/125*midterm + 60/100*final for (midterm, final) in class_en]
 
-    # TODO) Plot midterm/final scores as points
-
-    # TODO) Plot total scores as a histogram
+    # Plot midterm/final scores as points
+    plt.figure(1)
+    plt.grid(True)
+    plt.axis([0, 125, 0, 100])
+    plt.xlabel('Midterm scores')
+    plt.ylabel('Final scores')
+    plt.scatter(midterm_kr, final_kr, color='red', marker='o', label='Korean')
+    plt.scatter(midterm_en, final_en, color='blue', marker='+', label='English')
+    plt.legend()
+    
+    # Plot total scores as a histogram
+    plt.figure(2)
+    plt.xlim([0, 100])
+    plt.xlabel('Total scores')
+    plt.ylabel('The number of students')
+    plt.hist(total_kr, bins=20, range=(0, 100), alpha=0.3, color='red', label='Korean')
+    plt.hist(total_en, bins=20, range=(0, 100), alpha=0.3, color='blue', label='English')
+    plt.legend()
 
     plt.show()
