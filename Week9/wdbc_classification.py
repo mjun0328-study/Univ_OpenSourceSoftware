2df6f25dacd8e5df69488cabdd4d706716b5c26e
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-11-04 14:34:27 +0900

    Week9 Practice

--- Week9/wdbc_classification.py
+++ Week9/wdbc_classification.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
-from sklearn import (datasets, svm, metrics)
+from sklearn import (svm, metrics)
 from matplotlib.lines import Line2D # For the custom legend
 
 def load_wdbc_data(filename):
@@ -12,27 +14,33 @@ def load_wdbc_data(filename):
                          'radius error', 'texture error', 'perimeter error', 'area error', 'smoothness error', 'compactness error', 'concavity error', 'concave points error', 'symmetry error', 'fractal dimension error',
                          'worst radius', 'worst texture', 'worst perimeter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal dimension']
     wdbc = WDBCData()
-    with open(filename) as f:
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
         for line in f.readlines():
             items = line.split(',')
-            wdbc.target.append(items[1])        # TODO #1) Add the true label (0 for M / 1 for others)
-            wdbc.data.append(items[2:])         # TODO #1) Add 30 attributes (as floating-point numbers)
+            wdbc.target.append(0 if items[1] == 'M' else 1)        # Add the true label (0 for M / 1 for others)
+            wdbc.data.append([float(item) for item in items[2:]])  # Add 30 attributes (as floating-point numbers)
         wdbc.data = np.array(wdbc.data)
     return wdbc
 
 if __name__ == '__main__':
     # Load a dataset
-    wdbc = load_wdbc_data('data/wdbc.data')     # TODO #1) Implement 'load_wdbc_data()'
+    wdbc = load_wdbc_data('data/wdbc.data')
 
     # Train a model
-    model = svm.SVC()                           # TODO #2) Find a better classifier (SVC accuracy: 0.902)
+    model = svm.SVC()
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(wdbc.data, wdbc.target)
 
     # Test the model
     predict = model.predict(wdbc.data)
     accuracy = metrics.balanced_accuracy_score(wdbc.target, predict)
 
-    # TODO #3) Visualize the confusion matrix
+    # Visualize the confusion matrix
+    cm = metrics.confusion_matrix(wdbc.target, predict)
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=wdbc.target_names)
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    plt.show()
 
     # Visualize testing results
     cmap = np.array([(1, 0, 0), (0, 1, 0)])
